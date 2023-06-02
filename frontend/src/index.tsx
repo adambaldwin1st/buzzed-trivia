a8321bc71e4c22f5abe2bfd7f12e6991e807f055
adambaldwin <abaldwinva98@gmail.com>
2023-06-02 14:04:42 -0400

    Doin stuff

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,7 @@
-import React from "react";
-import { createRoot } from 'react-dom/client';
-import App from "./App";
-import Modal from "react-modal";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import {BrowserRouter} from "react-router-dom";
 
-Modal.setAppElement("#root");
-
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(<App />);
+ReactDOM.render(
+    <BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
