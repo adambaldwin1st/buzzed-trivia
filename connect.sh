89713143112fef9fdfe68716a898c01850f2b084
adambaldwin <abaldwinva98@gmail.com>
2023-06-03 17:14:56 -0400

    Adding stuff

--- connect.sh
+++ connect.sh
@@ -2,7 +2,7 @@
 
 # Specify the key pair file and EC2 instance details
 KEY_PAIR_FILE="~/Development/buzzed-trivia.pem"
-EC2_INSTANCE_IP="3.92.1.241"
+EC2_INSTANCE_IP="44.203.70.232"
 EC2_USERNAME="ubuntu"
 
 # SSH into the EC2 instance
